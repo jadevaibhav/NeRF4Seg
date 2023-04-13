6fbb46d8b0f525bdb80a461911476d39d2db969c
jadevaibhav <25821637+jadevaibhav@users.noreply.github.com>
2023-04-12 23:36:33 -0400

    wip:config handling for new models

--- train_nerf.py
+++ train_nerf.py
@@ -114,6 +114,7 @@ def main():
         )
 
     # Initialize a coarse-resolution model.
+    '''
     model_coarse = getattr(models, cfg.models.coarse.type)(
         num_encoding_fn_xyz=cfg.models.coarse.num_encoding_fn_xyz,
         num_encoding_fn_dir=cfg.models.coarse.num_encoding_fn_dir,
@@ -130,6 +131,28 @@ def main():
             num_encoding_fn_dir=cfg.models.fine.num_encoding_fn_dir,
             include_input_xyz=cfg.models.fine.include_input_xyz,
             include_input_dir=cfg.models.fine.include_input_dir,
+            use_viewdirs=cfg.models.fine.use_viewdirs,
+        )
+        model_fine.to(device)
+    '''
+    model_coarse = getattr(models, cfg.models.coarse.type)(
+        #num_encoding_fn_xyz=cfg.models.coarse.num_encoding_fn_xyz,
+        #num_encoding_fn_dir=cfg.models.coarse.num_encoding_fn_dir,
+        #include_input_xyz=cfg.models.coarse.include_input_xyz,
+        #include_input_dir=cfg.models.coarse.include_input_dir,
+        num_encoding_functions = cfg.models.coarse.num_encoding_functions,
+        use_viewdirs=cfg.models.coarse.use_viewdirs,
+    )
+    model_coarse.to(device)
+    # If a fine-resolution model is specified, initialize it.
+    model_fine = None
+    if hasattr(cfg.models, "fine"):
+        model_fine = getattr(models, cfg.models.fine.type)(
+            #num_encoding_fn_xyz=cfg.models.fine.num_encoding_fn_xyz,
+            #num_encoding_fn_dir=cfg.models.fine.num_encoding_fn_dir,
+            #include_input_xyz=cfg.models.fine.include_input_xyz,
+            #include_input_dir=cfg.models.fine.include_input_dir,
+            num_encoding_functions = cfg.models.fine.num_encoding_functions,
             use_viewdirs=cfg.models.fine.use_viewdirs,
         )
         model_fine.to(device)
